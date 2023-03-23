dbc158846a7143ca4ff96889a929a3776ee1626e
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-23 12:35:20 +0100

    Add delegate command

---
+++ Source/Padutronics.Commands/DelegateCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Padutronics.Commands;
+
+public sealed class DelegateCommand : Command
+{
+    private static readonly Func<bool> defaultCanExecuteMethod = () => true;
+
+    private readonly Func<bool> canExecuteMethod;
+    private readonly Action executeMethod;
+
+    public DelegateCommand(Action executeMethod) :
+        this(executeMethod, defaultCanExecuteMethod)
+    {
+    }
+
+    public DelegateCommand(Action executeMethod, Func<bool> canExecuteMethod)
+    {
+        this.canExecuteMethod = canExecuteMethod;
+        this.executeMethod = executeMethod;
+    }
+
+    public override bool CanExecute()
+    {
+        return canExecuteMethod();
+    }
+
+    public override void Execute()
+    {
+        executeMethod();
+    }
+}
---
+++ Source/Padutronics.Commands/DelegateCommand`1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Padutronics.Commands;
+
+public sealed class DelegateCommand<TParameter> : Command<TParameter>
+{
+    private static readonly Func<TParameter, bool> defaultCanExecuteMethod = _ => true;
+
+    private readonly Func<TParameter, bool> canExecuteMethod;
+    private readonly Action<TParameter> executeMethod;
+
+    public DelegateCommand(Action<TParameter> executeMethod) :
+        this(executeMethod, defaultCanExecuteMethod)
+    {
+    }
+
+    public DelegateCommand(Action<TParameter> executeMethod, Func<TParameter, bool> canExecuteMethod)
+    {
+        this.canExecuteMethod = canExecuteMethod;
+        this.executeMethod = executeMethod;
+    }
+
+    public override bool CanExecute(TParameter parameter)
+    {
+        return canExecuteMethod(parameter);
+    }
+
+    public override void Execute(TParameter parameter)
+    {
+        executeMethod(parameter);
+    }
+}
