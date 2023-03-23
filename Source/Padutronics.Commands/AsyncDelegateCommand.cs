51ec6fec341b052d4522bcc08fb0dc659f595dd3
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-23 12:36:00 +0100

    Add delegate async command

---
+++ Source/Padutronics.Commands/AsyncDelegateCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Padutronics.Commands;
+
+public sealed class AsyncDelegateCommand : AsyncCommand
+{
+    private static readonly Func<bool> defaultCanExecuteMethod = () => true;
+
+    private readonly Func<bool> canExecuteMethod;
+    private readonly Func<Task> executeMethod;
+
+    public AsyncDelegateCommand(Func<Task> executeMethod) :
+        this(executeMethod, defaultCanExecuteMethod)
+    {
+    }
+
+    public AsyncDelegateCommand(Func<Task> executeMethod, Func<bool> canExecuteMethod)
+    {
+        this.canExecuteMethod = canExecuteMethod;
+        this.executeMethod = executeMethod;
+    }
+
+    public override bool CanExecute()
+    {
+        return !IsExecuting && canExecuteMethod();
+    }
+
+    public override async Task ExecuteAsync()
+    {
+        try
+        {
+            IsExecuting = true;
+
+            RaiseCanExecuteChanged();
+
+            await executeMethod();
+        }
+        finally
+        {
+            IsExecuting = false;
+
+            RaiseCanExecuteChanged();
+        }
+    }
+}
---
+++ Source/Padutronics.Commands/AsyncDelegateCommand`1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Padutronics.Commands;
+
+public sealed class AsyncDelegateCommand<TParameter> : AsyncCommand<TParameter>
+{
+    private static readonly Func<TParameter, bool> defaultCanExecuteMethod = _ => true;
+
+    private readonly Func<TParameter, bool> canExecuteMethod;
+    private readonly Func<TParameter, Task> executeMethod;
+
+    public AsyncDelegateCommand(Func<TParameter, Task> executeMethod) :
+        this(executeMethod, defaultCanExecuteMethod)
+    {
+    }
+
+    public AsyncDelegateCommand(Func<TParameter, Task> executeMethod, Func<TParameter, bool> canExecuteMethod)
+    {
+        this.canExecuteMethod = canExecuteMethod;
+        this.executeMethod = executeMethod;
+    }
+
+    public override bool CanExecute(TParameter parameter)
+    {
+        return !IsExecuting && canExecuteMethod(parameter);
+    }
+
+    public override async Task ExecuteAsync(TParameter parameter)
+    {
+        try
+        {
+            IsExecuting = true;
+
+            RaiseCanExecuteChanged();
+
+            await executeMethod(parameter);
+        }
+        finally
+        {
+            IsExecuting = false;
+
+            RaiseCanExecuteChanged();
+        }
+    }
+}
